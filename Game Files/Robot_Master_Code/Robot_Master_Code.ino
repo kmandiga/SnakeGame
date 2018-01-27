9347ab8e2537f20cc61b96eec237150523ffc1f2
kmandiga <kmandiga@uh.edu>
2018-01-26 21:21:11 -0600

    Master Code File
    Master Code for the robots.

---
+++ Game Files/Robot_Master_Code/Robot_Master_Code.ino
@@ -0,0 +1,15 @@
+/*
+ * Master code for the robot. Will include the logic
+ * for brain, food, tail, inactive, cleanup. Will be able to
+ * switch states based on I/O from ESP8266.
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
